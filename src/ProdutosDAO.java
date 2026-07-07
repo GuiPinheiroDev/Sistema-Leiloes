f1ffa85423393f573f2fce3df9a7352ad1116bce
Guilherme Pinheiro <guipinheiro2501@gmail.com>
2026-07-07 17:29:13 -0300

    Listagem de produtos concluído

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -43,12 +43,29 @@ public class ProdutosDAO {
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
+    
+    String sql = "SELECT * FROM produtos";
+    conn = new conectaDAO().connectDB();
+
+    try{
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while(resultset.next()){
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            listagem.add(produto);
+        }
+    }catch(Exception e){
+        JOptionPane.showMessageDialog(null,e.getMessage());
     }
-    
-    
-    
-        
+    return listagem;
+    }
+          
 }
 
